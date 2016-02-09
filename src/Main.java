aeb0b0f69f81fbc60662597cf94c2bff7999768d
Ricky Martinez <ricky.0571645@gmail.com>
2016-02-09 11:09:35 -0800

    JavaApplication

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+/*
+ * This application will communicate with an Arduino
+ */
+public class Main 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Arduino Communication Program");
+		
+	}
+}
