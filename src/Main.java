781b11b60fc801d061380806b765d55d1e2d577e
Ricky Martinez <ricky.0571645@gmail.com>
2016-02-10 23:02:25 -0800

    Java communicating with Arduino Successful
    I will comment the code on the next upload

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,44 @@ public class Main
 {
 	public static void main (String args[])
 	{
+		boolean arduinoConnected = false;
+		int arduinoPort = 0;
+		int i = 0;
+		
+		SerialPort[] ports = SerialPort.getCommPorts();
+		System.out.println("To display the ports that we have available: ");
+		for(SerialPort port : ports)
+		{
+			System.out.println(port.getSystemPortName() + "\n");
+			if(port.getSystemPortName().equalsIgnoreCase("COM3"))
+			{
+				arduinoConnected = true;
+				arduinoPort = i;
+			}
+			i++;
+		}
+		
+		if(arduinoConnected)
+		{
+			SerialPort serialPort = ports[arduinoPort];
+			//check whether port can open
+			if(serialPort.openPort())
+				System.out.println("Port opened successfully.");
+			else 
+			{
+				System.out.println("Unable to open the port.");
+				return;
+			}
+			byte buffer[] = {'n'};
+			
+			int bytesToWrite = buffer.length;
+			
+			serialPort.writeBytes(buffer, bytesToWrite);
+			
+			
+			
+		}
+		
 		
 	}
 }
