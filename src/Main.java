a919445fb23b21112e2274be4043709e664a05a1
Ricky Martinez <ricky.0571645@gmail.com>
2016-02-10 20:56:14 -0800

    Update
    Included the serial communication jar file to read in from the ports

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-
-/*
- * This application will communicate with an Arduino
- */
+import com.fazecast.jSerialComm.*;
 public class Main 
 {
-	public static void main(String args[])
+	public static void main (String args[])
 	{
-		System.out.println("Arduino Communication Program");
 		
-		System.out.println("Testing Github Compatablity");
-		
-		System.out.println("Testing whether Github can branch");
 	}
 }
